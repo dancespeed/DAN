a5095439618a1b7bb0d8f7470a9f06436b59e6be
Dancespeed <dancespeed260578@gmail.com>
2026-07-20 17:50:58 +0300

    feat: add SequenceStep definition

--- src/sequencer/sequencer.hpp
+++ src/sequencer/sequencer.hpp
@@ -1,9 +1,22 @@
 #pragma once
 
+#include <stdint.h>
+
 #include "event/event.hpp"
 
 namespace Sequencer
 {
+    enum class Command : uint8_t
+    {
+        PublishEvent
+    };
+
+    struct SequenceStep
+    {
+        Command command;
+        Event   event;
+    };
+
     void Init();
 
     void HandleEvent(const Event& event);
